6c547b689120d3b95db9c8c5e127d55a3505c4d5
Rodrigo do Nascimento <rodrigob.dev@outlook.com>
2021-02-28 01:31:35 -0300

    first commit

---
+++ src/pages/document.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Document, {
+  DocumentInitialProps,
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript
+} from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.query.selector
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render(): JSX.Element {
+    return (
+        <div>
+          <Main />
+          <NextScript />
+        </div>
+    )
+  }
+}
